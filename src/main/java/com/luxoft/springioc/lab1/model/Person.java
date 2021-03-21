ee84f4a2fc783623dceeb839c57d44fe5718c149
glaschenko <a.glaschenko@gmail.com>
2021-03-21 23:32:27 +0400

    homework 1

--- src/main/java/com/luxoft/springioc/lab1/model/Person.java
+++ src/main/java/com/luxoft/springioc/lab1/model/Person.java
@@ -4,4 +4,9 @@ public interface Person {
     public void setName(String name);
     public String getName ();
     public void sayHello(Person person);
+
+
+    Country getCountry();
+
+    void setCountry(Country country);
 }
